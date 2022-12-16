4001df1484d64aa3fc88a218f1c8119f7f053fa1
Bartosz Wasilewski <bartosz.wasilewski@itexcellence.pl>
2022-12-16 20:25:02 +0100

    + MED-09

---
+++ src/test/java/com/github/bartosz/medicalclinic/service/PatientServiceTest.java
@@ -0,0 +1,129 @@
+package com.github.bartosz.medicalclinic.service;
+
+import com.github.bartosz.medicalclinic.model.Patient;
+import com.github.bartosz.medicalclinic.repository.PatientRepository;
+import com.github.bartosz.medicalclinic.util.PatientUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class PatientServiceTest {
+
+    private PatientService patientService;
+    @MockBean
+    private PatientRepository patientRepository;
+
+    @BeforeEach
+    void setup() {
+        patientService = new PatientService(patientRepository);
+    }
+
+    @Test
+    void getAllPatientsTest() {
+        var patients = List.of(PatientUtils.buildPatient());
+        when(patientRepository.findAll()).thenReturn(patients);
+
+        var result = patientService.getAllPatients();
+
+        assertNotNull(result);
+        assertEquals(1, result.size());
+        checkPatients(patients, result);
+    }
+
+    @Test
+    void getPatientByEmailTest() {
+        var patient = PatientUtils.buildPatient();
+        when(patientRepository.findByEmail(anyString())).thenReturn(Optional.of(patient));
+
+        var result = patientService.getPatientByEmail("test@gmail.com");
+
+        assertNotNull(result);
+        checkPatient(patient, result);
+    }
+
+    @Test
+    void getPatientByIdTest() {
+        var patient = PatientUtils.buildPatient();
+        when(patientRepository.getPatientById(anyLong())).thenReturn(Optional.of(patient));
+
+        var result = patientService.getPatientById(1);
+
+        assertNotNull(result);
+        checkPatient(patient, result);
+    }
+
+    @Test
+    void addPatientTest() {
+        var patient = PatientUtils.buildPatient();
+        when(patientRepository.findByEmail(anyString())).thenReturn(Optional.empty());
+        doNothing().when(patientRepository).save(any());
+
+        patientService.addPatient(patient);
+
+        verify(patientRepository).save(patient);
+    }
+
+    @Test
+    void editPatientTest() {
+        var patient = PatientUtils.buildPatient();
+        var email = patient.getEmail();
+        when(patientRepository.findByEmail(anyString())).thenReturn(Optional.of(patient));
+        doNothing().when(patientRepository).update(anyString(), any(Patient.class));
+
+        patientService.editPatient(email, patient);
+
+        verify(patientRepository).update(email, patient);
+    }
+
+    @Test
+    void deletePatientByEmailTest() {
+        var email = "test@gmail.com";
+        doNothing().when(patientRepository).deleteByEmail(anyString());
+
+        patientService.deletePatientByEmail(email);
+
+        verify(patientRepository).deleteByEmail(email);
+    }
+
+    @Test
+    void editPasswordTest() {
+        var patient = PatientUtils.buildPatient();
+        var email = "test@gmail.com";
+        var password = "test1";
+        when(patientRepository.findByEmail(anyString())).thenReturn(Optional.of(patient));
+        doNothing().when(patientRepository).update(anyString(), any(Patient.class));
+
+        patient.setPassword(password);
+
+        verify(patientRepository).update(email, patient);
+    }
+
+    private void checkPatients(List<Patient> expectedPatients, List<Patient> actualPatients) {
+        int i = 0;
+        for (var expectedPatient : expectedPatients) {
+            checkPatient(expectedPatient, actualPatients.get(i));
+            i++;
+        }
+    }
+
+    private void checkPatient(Patient expectedPatient, Patient actualPatient) {
+
+        assertEquals(expectedPatient.getEmail(), actualPatient.getEmail());
+        assertEquals(expectedPatient.getPassword(), actualPatient.getPassword());
+        assertEquals(expectedPatient.getFirstName(), actualPatient.getFirstName());
+        assertEquals(expectedPatient.getLastName(), actualPatient.getLastName());
+        assertEquals(expectedPatient.getPhoneNumber(), actualPatient.getPhoneNumber());
+        assertEquals(expectedPatient.getIdCardNo(), actualPatient.getIdCardNo());
+        assertEquals(expectedPatient.getBirthday(), actualPatient.getBirthday());
+    }
+}
---
+++ src/test/java/com/github/bartosz/medicalclinic/util/PatientUtils.java
@@ -0,0 +1,22 @@
+package com.github.bartosz.medicalclinic.util;
+
+import com.github.bartosz.medicalclinic.model.Patient;
+
+import java.time.LocalDate;
+
+public final class PatientUtils {
+
+    private PatientUtils() {}
+
+    public static Patient buildPatient() {
+        return Patient.builder()
+                .email("test@gmail.com")
+                .password("test")
+                .birthday(LocalDate.now())
+                .firstName("Test1")
+                .lastName("Test2")
+                .phoneNumber("821222838")
+                .idCardNo(1)
+                .build();
+    }
+}
