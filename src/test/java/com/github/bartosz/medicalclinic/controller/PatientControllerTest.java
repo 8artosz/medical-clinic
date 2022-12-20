34bf03715f2283a76cd605b15e25f5e5dbbf817e
Bartosz Wasilewski <bartosz.wasilewski@itexcellence.pl>
2022-12-20 11:54:39 +0100

    + MED-05

---
+++ src/test/java/com/github/bartosz/medicalclinic/controller/PatientControllerTest.java
@@ -0,0 +1,108 @@
+package com.github.bartosz.medicalclinic.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.bartosz.medicalclinic.util.PatientUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+class PatientControllerTest {
+
+    @Autowired
+    private PatientController patientController;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    private static final String ROOT_PATH = "$";
+    private static final String EMAIL_PATH = "$[0].email";
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getAllPatientsTest() throws Exception {
+        var patient = PatientUtils.buildPatient();
+        patientController.addPatient(patient);
+        mockMvc.perform(get("/patients"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath(ROOT_PATH).isArray())
+                .andExpect(jsonPath(EMAIL_PATH).value(patient.getEmail()));
+    }
+
+    @Test
+    void getPatientByEmailTest() throws Exception {
+        var patient = PatientUtils.buildPatient();
+        patientController.addPatient(patient);
+        mockMvc.perform(get("/patients/{email}", patient.getEmail()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value(patient.getEmail()));
+    }
+
+    @Test
+    void getPatientByIdTest() throws Exception {
+        var patient = PatientUtils.buildPatient();
+        patientController.addPatient(patient);
+        mockMvc.perform(get("/patients/id")
+                        .param("id", "1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value(patient.getEmail()));
+    }
+
+    @Test
+    void addPatientTest() throws Exception {
+        var patient = PatientUtils.buildPatient();
+        mockMvc.perform(post("/patients")
+                .content(objectMapper.writeValueAsString(patient))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void deletePatientByEmailTest() throws Exception {
+        var patient = PatientUtils.buildPatient();
+        patientController.addPatient(patient);
+        mockMvc.perform(delete("/patients/{email}", patient.getEmail()))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void editPatientTest() throws Exception {
+        var patient = PatientUtils.buildPatient();
+        patientController.addPatient(patient);
+        mockMvc.perform(put("/patients/{email}",patient.getEmail())
+                .content(objectMapper.writeValueAsString(patient))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void editPasswordTest() throws Exception {
+        var patient = PatientUtils.buildPatient();
+        patientController.addPatient(patient);
+        mockMvc.perform(patch("/patients/{email}",patient.getEmail())
+                        .content(objectMapper.writeValueAsString("asd123"))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
